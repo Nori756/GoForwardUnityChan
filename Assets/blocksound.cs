6655d3289725c8346a589157417e53195c7a8469
NORI756 <kiyohide826@gmail.com>
2019-06-12 22:21:14 +0900

    Lesson 7 課題

---
+++ Assets/blocksound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocksound : MonoBehaviour {
+
+    private AudioSource audioSource;
+   
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    { if(collision.gameObject.tag =="blockTag")
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+                
+   
+        }
+    }
+}
